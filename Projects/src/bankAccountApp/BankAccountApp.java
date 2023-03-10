576bc1a44348bfaa8d808bc449abde42218d7ed6
Dino Himaj <dinohimaj@rocketmail.com>
2023-03-10 21:54:21 +0100

    added reading .csv file functionality

--- Projects/src/bankAccountApp/BankAccountApp.java
+++ Projects/src/bankAccountApp/BankAccountApp.java
@@ -1,18 +1,35 @@
 package bankAccountApp;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BankAccountApp {
     public static void main(String[] args){
-        Checking chkacc1 = new Checking("Dino Himaj", "123456789", 1500);
-        Savings savacc1 = new Savings("Richie Rich", "123456798", 2500);
+        List<Account> accounts = new LinkedList<Account>();
 
-        savacc1.compound();
-        System.out.println("******************************************\n");
-
-        chkacc1.showInfo();
-        System.out.println("*********************");
-        savacc1.showInfo();
 
         //Read a CSV file then create new accounts based on that data
+        String file = "C:\\Users\\dinoh\\Desktop\\Java-Small-Projects\\Projects\\src\\bankAccountApp\\customers.csv";
+
+        List<String[]> newAccountHolder = utilities.CSV.read(file);
+        for(String[] accountHolder : newAccountHolder){
+            String name = accountHolder[0];
+            String sSN = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit = Double.parseDouble(accountHolder[3]);
+            if(accountType.equals("Savings")){
+                accounts.add(new Savings(name, sSN, initDeposit));
+            }else if (accountType.equals("Checkings")){
+                accounts.add(new Checking(name, sSN, initDeposit));
+            }else{
+                System.out.println("ERROR READING ACCOUNT TYPE");
+            }
+        }
+        for(Account acc : accounts){
+            System.out.println("\n*******************************");
+            acc.showInfo();
+        }
 
     }
+
 }
